ea2d1b41d92e750fe1750daae4741f2b40b548c8
Васильев Антон Максимович <ip814s03@mwp01.csc.local>
2019-05-27 14:28:12 +0700

    Add files from lab3

---
+++ src/geometry.h
@@ -0,0 +1,40 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+#include <cmath>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct circles {
+    int num;
+    char fig[10];
+    float x;
+    float y;
+    float r;
+} circle;
+
+typedef struct triangles {
+    int num;
+    char fig[10];
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float x3;
+    float y3;
+} triangle;
+
+float Dist(int x1, int y1, int x2, int y2);
+void CircleIntersects(circle* circles, triangle* triangles, int Circ, int Trig);
+void Fileread(FILE* input, circle* circles);
+float Perimetr(float x1, float y1, float x2, float y2, float x3, float y3);
+float Area(float x1, float y1, float x2, float y2, float x3, float y3, float P);
+void TriangleIntersect(circle* circles, triangle* triangles, int Circ,
+    int Trig);
+void ScanFile(FILE* input, circle* circles, triangle* triangles, char* c,
+    char* t);
+float Height(float x1, float y1, float x2, float y2, float x3, float y3,
+    float P);
+
+#endif
